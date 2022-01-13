43b876c69a8b3d181aa3bb0dba625cc9a34b922b
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-01-13 12:24:01 +0100

    renamed name variables and added initial text

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
+// Initialize document elements
+const displayName = document.getElementById("name");
+const displayTotal = document.getElementById("total");
+const commentPara = document.getElementById("commentaartekst");
+
 // Get the student name
-let studentName = document.getElementById("name");
-let displayTotal = document.getElementById("total");
-studentName.innerText = prompt("Naam van de leerling:", '');
+const studentName = prompt("Naam van de leerling:", '')
+displayName.innerText = studentName;
 displayTotal.innerHTML = '';
 
 // Get all grades
@@ -14,7 +18,7 @@ let gradeTheory = prompt("Percentage voor theorie:");
 gradeTotal = (Number(gradeEarRhythm) + Number(gradeEarMelodic) + Number(gradeSinging) + Number(gradeTheory))/4;
 
 // Create Rapport Card comment
-
+commentPara.innerHTML = `Dit is een ... rapport, ${studentName}`;
 
 // Insert rapport card comment into web page
 displayTotal.innerHTML = `${gradeTotal} %`;
