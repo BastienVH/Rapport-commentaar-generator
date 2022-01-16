6a1925f81eb37d71ae69c796d85d87082adfcc88
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-01-16 19:07:54 +0100

    added link to js file

---
+++ scripts/kinderkoor.js
@@ -0,0 +1,9 @@
+// Initialize document elements
+const displayName = document.getElementById("name");
+const displayTotal = document.getElementById("total");
+const commentPara = document.getElementById("commentaartekst");
+
+// Get the student name
+// const studentName = prompt("Naam van de leerling:", '')
+// displayName.innerText = studentName;
+// displayTotal.innerHTML = '';
