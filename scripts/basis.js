50362d0e1e260c794a5a21b676108a7af9af3e10
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-01-21 09:59:19 +0100

    fixed issues which I already forgot

--- scripts/basis.js
+++ scripts/basis.js
@@ -9,15 +9,30 @@ const generateBtn = document.getElementById("generateButton");
 generateBtn.addEventListener("click", generateBasisFeedback);
 
 // Set correct gendered words
-let aanwijzendVnw = "";
-let aanwijzendVnwCap = "";
-let bezittVnw = "";
-let bezittVnwCap = "";
-let lijdVnw = "";
+var aanwijzendVnw = "";
+var aanwijzendVnwCap = "";
+var bezittVnw = "";
+var bezittVnwCap = "";
+var lijdVnw = "";
 
 function generateBasisFeedback() {
   // Get selected values from the form
   let gender = returnSelection('gender');
+  
+  if (gender === 'jongen') {
+    aanwijzendVnw = "hij";
+    aanwijzendVnwCap = "Hij";
+    bezittVnw = "zijn";
+    bezittVnwCap = "Zijn";
+    lijdVnw = "hem";
+  } else {
+    aanwijzendVnw = "ze";
+    aanwijzendVnwCap = "Ze";
+    bezittVnw = "haar";
+    bezittVnwCap = "Haar";
+    lijdVnw = "haar";
+  }
+
   displayName = document.getElementById("name");
   let beschrijving1 = selectBeschr1.value;
   let beschrijving2 = selectBeschr2.value;
@@ -42,19 +57,6 @@ function generateBasisFeedback() {
   let selectPuntenIntro1 = document.getElementById(`punten_${punten}_selector1`).value;
   let selectPuntenIntro2 = document.getElementById(`punten_${punten}_selector2`).value;
 
-  if (gender === 'jongen') {
-    aanwijzendVnw = "hij";
-    aanwijzendVnwCap = "Hij";
-    bezittVnw = "zijn";
-    bezittVnwCap = "Zijn";
-    lijdVnw = "hem";
-  } else {
-    aanwijzendVnw = "ze";
-    aanwijzendVnwCap = "Ze";
-    bezittVnw = "haar";
-    bezittVnwCap = "Haar";
-    lijdVnw = "haar";
-  }
 
   // Associative arrays with feedback
 const dictSamenwerken = {
@@ -114,13 +116,6 @@ const dictPunten = {
   schitterend : `Het rapport van ${displayName.value} staat weer vol schitterende resultaten, zoals voor ${selectPuntenIntro1} en ${selectPuntenIntro2}. Hier mag ${aanwijzendVnw} erg trots op zijn.\n`
 }
 
-const dictWerkpunten = {
-  inzet : `Ik denk dat ${displayName.value} thuis nog iets meer moet werken voor school.`,
-  beetje : `De komende periode kan ${aanwijzendVnw} nog wat extra aandacht schenken aan lezen / luisteren / taalsystematiek / getallenkennis en bewerkingen / meten en metend rekenen / wereldoriëntatie / godsdienst en lezen / luisteren / taalsystematiek / getallenkennis en bewerkingen / meten en metend rekenen / wereldoriëntatie / godsdienst`,
-  wiskunde : `Wiskunde / Bewerkingen / Metend rekenen en meetkunde blijft een werkpunt. Het is belangrijk om voldoende uitleg te komen vragen in de klas.`,
-  spelling : `De dictees van ${displayName.value} zijn onvoldoende. Het is belangrijk om elke dag de woorden van het woordpakket te schrijven en fouten te verbeteren. Op de website van de school vind je ingesproken dictees van elk woordpakket die kunnen helpen bij het oefenen.`,
-  wo_godsdienst : `${bezittVnwCap} resultaten voor w.o. en godsdienst zijn deze periode nogal zwak. Dit zijn onderdelen waar ${aanwijzendVnw} thuis voor moet studeren. ${aanwijzendVnwCap} moet dan elke dag leren en hoofdgedachten noteren.`
-}
 const dictAvi = {
   gelijk : `Het leesniveau van ${displayName.value} is niet gestegen ten opzichte van september. Dat is jammer.  Het is noodzakelijk dat ${displayName.value} elke dag (luidop) leest zodat ${aanwijzendVnw} op het einde van dit schooljaar AVI9 behaalt. De teksten die de leerlingen moeten verwerken voor de lessen taal en wereldoriëntatie worden steeds langer en moeilijker, vlot kunnen lezen is daarom enorm belangrijk.\n`,
   gestegen_zwak : `Het leesniveau van ${displayName.value} is gestegen naar Avi 5 / 6 / 7 , dus ${displayName.value} kan beginnen oefenen op Avi 6 / 7 / 8. De teksten die de leerlingen moeten verwerken voor de lessen taal en wereldoriëntatie worden steeds langer en moeilijker, vlot kunnen lezen is daarom enorm belangrijk.\n`,
@@ -170,9 +165,11 @@ function getWerkpunten() {
 
 function getWerkpuntenFullText(nodelist) {
   // Define dictionary with feedback
+  let selectWerkpuntenBeetje1 = document.getElementById(`werkpunten_beetje_selector1`).value;
+  let selectWerkpuntenBeetje2 = document.getElementById(`werkpunten_beetje_selector2`).value;
   const dictWerkpunten = {
     inzet : `Ik denk dat ${displayName.value} thuis nog iets meer moet werken voor school.`,
-    beetje : `De komende periode kan ${aanwijzendVnw} nog wat extra aandacht schenken aan lezen / luisteren / taalsystematiek / getallenkennis en bewerkingen / meten en metend rekenen / wereldoriëntatie / godsdienst en lezen / luisteren / taalsystematiek / getallenkennis en bewerkingen / meten en metend rekenen / wereldoriëntatie / godsdienst`,
+    beetje : `De komende periode kan ${aanwijzendVnw} nog wat extra aandacht schenken aan ${selectWerkpuntenBeetje1} en ${selectWerkpuntenBeetje2}.`,
     wiskunde : `Wiskunde / Bewerkingen / Metend rekenen en meetkunde blijft een werkpunt. Het is belangrijk om voldoende uitleg te komen vragen in de klas.`,
     spelling : `De dictees van ${displayName.value} zijn onvoldoende. Het is belangrijk om elke dag de woorden van het woordpakket te schrijven en fouten te verbeteren. Op de website van de school vind je ingesproken dictees van elk woordpakket die kunnen helpen bij het oefenen.`,
     wo_godsdienst : `${bezittVnwCap} resultaten voor w.o. en godsdienst zijn deze periode nogal zwak. Dit zijn onderdelen waar ${aanwijzendVnw} thuis voor moet studeren. ${aanwijzendVnwCap} moet dan elke dag leren en hoofdgedachten noteren.`
