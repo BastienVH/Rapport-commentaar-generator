b7c841ed69561a2c3b6cf4c274a1fd90aacc8aad
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-03-28 10:57:17 +0200

    remove unused dictionaries and add dict for Komma

--- scripts/rapport3.js
+++ scripts/rapport3.js
@@ -31,119 +31,24 @@ function generateBasisFeedback() {
   }
 
   displayName = document.getElementById("name");
-  let samenwerken = returnSelection("samenwerken");
-  let individueel = returnSelection("individueel")
-  let uitleg = returnSelection("uitleg");
-  let klassikaal = returnSelection("klassikaal");
-  let klGroepje = returnSelection("klgroepje");
-  let helpen = returnSelection("helpen");
-  let schrijven = returnSelection("schrijven");
-  let bank = returnSelection("bank");
-  let punten = returnSelection("punten");
-  let werkpuntenLijst = getWerkpunten();
-  let werkpunten = getWerkpuntenFullText(werkpuntenLijst);
-  let avi = returnSelection("avi");
-  let tafels = returnSelection("tafels");
-  let voorlezen = returnSelection("voorlezen");
-  let slot = returnSelection("slot");
-
-  //read value for selectors
-  let selectPuntenIntro1 = "";
-  let selectPuntenIntro2 = "";
-
-  if (punten === "goed" || punten === "schitterend") {
-    selectPuntenIntro1 = document.getElementById(`punten_${punten}_selector1`).value;
-    selectPuntenIntro2 = document.getElementById(`punten_${punten}_selector2`).value;
-  }
-
+  let kommagetallen = returnSelection("kommagetallen");
+  // let kloklezen = returnSelection("klok");
+  // let dictee = returnSelection("dictee");
+  // let leerhouding = returnSelection("leerhouding");
+  // let groepswerk = returnSelection("groepswerk");
 
   // Associative arrays with feedback
-const dictSamenwerken = {
-  moeilijk_alleen : `${displayName.value} is nog aan het leren om samen te werken in groep. De anderen hebben niet altijd dezelfde ideeën, en dat vindt ${aanwijzendVnw} moeilijk om mee om te gaan. In plaats van een discussie aan te gaan en samen naar een oplossing te zoeken, zal ${displayName.value} zich dan terugtrekken en alleen beginnen werken.\n`,
-  moeilijk_ruzie : `Tijdens groepswerk wil ${aanwijzendVnw} graag dat alles op ${bezittVnw} manier gebeurt, dit botst soms met de ideeën van anderen. Door vaak in groep te werken leert ${aanwijzendVnw} stilaan om op te komen voor ${bezittVnw} eigen mening en toch ook rekening te houden met de anderen.\n`,
-  moeilijk_spelen : `Bij een groepswerk wil ${displayName.value} graag de sfeermaker van de groep zijn. Zich amuseren is dan belangrijker dan het resultaat van het groepswerk. Nochtans kan ${aanwijzendVnw} dit wel wanneer ik ${lijdVnw} hierop wijs.\n`,
-  gemiddeld : `In een groepje met zelfgekozen klasgenoten kan ${displayName.value} goed samenwerken.\n`,
-  goed : `${aanwijzendVnwCap} kan goed in een groep samenwerken. ${aanwijzendVnwCap} zorgt ervoor dat er geen ruzie ontstaat en komt tegelijkertijd ook op voor ${bezittVnw} eigen mening. Knap!\n`
-}
-
-const dictIndividueel = {
-  moeilijk : `Tijdens zelfstandig werk raakt ${displayName.value} makkelijk afgeleid. Dan begint ${aanwijzendVnw} te praten met de leerlingen die bij ${lijdVnw} in de buurt zitten.\n`,
-  gemiddeld : `${displayName.value} kan op sommige momenten heel flink zelfstandig werken. Op andere dagen lukt het minder goed. Dan wil ${aanwijzendVnw} liever babbelen met ${bezittVnw} buurtje.\n`,
-  goed : `Bij zelfstandig werken doet ${displayName.value} goed ${bezittVnw} best. ${aanwijzendVnwCap} werkt rustig en flink aan ${bezittVnw} oefeningen.\n`
-}
-
-const dictUitleg = {
-  moeilijk : `${aanwijzendVnwCap} komt zelden uit zichzelf extra uitleg vragen wanneer iets moeilijk gaat. Nochtans help ik ${lijdVnw} met plezier.\n`,
-  gemiddeld : `${aanwijzendVnwCap} vraagt soms extra uitleg wanneer ${aanwijzendVnw} iets niet begrijpt. Dat mag ${aanwijzendVnw} nog vaker doen.\n`,
-  goed : `${aanwijzendVnwCap} komt uitleg vragen wanneer ${aanwijzendVnw} iets niet begrijpt. Dat is een goede leerhouding!\n`
-}
-
-const dictKlassikaal = {
-  gemiddeld_spelen : `${aanwijzendVnwCap} wil graag opletten in de klas, maar wil ook graag plezier maken met ${bezittVnw} klasgenoten. ${aanwijzendVnwCap} moet goed onthouden dat je speelt op de speelplaats en werkt in de klas. Er zijn meer en meer momenten waarop ${displayName.value} goed meewerkt. Toch moet ik ${lijdVnw} regelmatig weer bij de les houden.\n`,
-  gemiddeld_dromen : `Tijdens klassikaal werk doet ${displayName.value} ${bezittVnw} best, maar ${bezittVnw} gedachten durven wel eens af te dwalen. ${aanwijzendVnwCap} heeft alle uitleg nochtans echt nodig.\n`,
-  goed : `Tijdens klassikaal werk denkt ${aanwijzendVnw} goed mee na en steekt ${aanwijzendVnw} enthousiast ${bezittVnw} vinger op.\n`
-}
-const dictKlGroepje = {
-  goed : `${displayName.value} krijgt regelmatig extra uitleg in een klein groepje. Dan doet ${aanwijzendVnw} goed mee.\n`,
-  niet_goed : `${displayName.value} krijgt regelmatig extra uitleg in een klein groepje. Dat doen we om ${lijdVnw} zo veel mogelijk te helpen. Jammer genoeg doet ${displayName.value} niet elke keer goed mee. ${aanwijzendVnwCap} heeft nochtans echt nood aan die extra begeleiding.\n`,
-  nvt : ""
-}
-
-const dictHelpen = {
-  juf : `Als er tijd is om de juf te helpen, doet ${displayName.value} dat met plezier. ${aanwijzendVnw} is erg behulpzaam.\n`,
-  klasgenoten : `${displayName.value} helpt graag klasgenoten als daar tijd voor is. ${aanwijzendVnw} is een goede helper.\n`,
-  nvt : ``
-}
-
-const dictSchrijven = {
-  slordig : `${displayName.value} werkt erg slordig. Door mooier te schrijven en netter om te gaan met materiaal, is het nochtans aangenamer om te studeren.\n`,
-  moeilijk : `Mooi schrijven blijft een werkpunt. ${aanwijzendVnwCap} moet hier extra op te letten wanneer ${aanwijzendVnw} zinnen schrijft die ${aanwijzendVnw} nadien nog eens moet lezen, bijvoorbeeld in ${bezittVnw} agenda of omdat ${aanwijzendVnw} moet dit studeren. Ook op toetsen is het belangrijk om mooi te schrijven, zodat de leerkracht alles goed kan lezen.\n`,
-  wisselend : `De laatste tijd schrijft ${displayName.value} regelmatig wat slordiger. ${aanwijzendVnwCap} kan nochtans heel mooi schrijven als ${aanwijzendVnw} er aandacht aan schenkt.\n`,
-  mooi : `${displayName.value} kan zeer netjes schrijven.\n`,
-  nvt : ``,
-}
-
-const dictBank = {
-  slordig : `Door de rommel in ${bezittVnw} bank, is het soms moeilijk om snel het juiste werkschrift terug te vinden. Alles een vaste plaats geven en alles meteen op diezelfde plaats terugleggen, kan helpen.\n`,
-  nvt : ``
-}
-
-const dictPunten = {
-  slecht : `SLECHT\n`,
-  matig : `De resultaten op het rapport van ${displayName.value} kunnen beter.\n`,
-  goed : `Er staan weer een aantal mooie resultaten op het rapport van ${displayName.value}, zoals voor ${selectPuntenIntro1} en ${selectPuntenIntro2}. Hier mag ${aanwijzendVnw} erg trots op zijn.\n`,
-  schitterend : `Het rapport van ${displayName.value} staat weer vol schitterende resultaten, zoals voor ${selectPuntenIntro1} en ${selectPuntenIntro2}. Hier mag ${aanwijzendVnw} erg trots op zijn.\n`
-}
-
-const dictAvi = {
-  gelijk : `Het leesniveau van ${displayName.value} is niet gestegen ten opzichte van september. Dat is jammer.  Het is noodzakelijk dat ${displayName.value} elke dag (luidop) leest zodat ${aanwijzendVnw} op het einde van dit schooljaar AVI 9 behaalt. De teksten die de leerlingen moeten verwerken voor de lessen taal en wereldoriëntatie worden steeds langer en moeilijker, vlot kunnen lezen is daarom enorm belangrijk.\n`,
-  gestegen_zwak : `Het leesniveau van ${displayName.value} is gestegen naar AVI 5 / 6 / 7 , dus ${displayName.value} kan beginnen oefenen op AVI 6 / 7 / 8. De teksten die de leerlingen moeten verwerken voor de lessen taal en wereldoriëntatie worden steeds langer en moeilijker, vlot kunnen lezen is daarom enorm belangrijk.\n`,
-  gestegen_goed : `Het leesniveau van ${displayName.value} is gestegen, goed zo! ${displayName.value} kan beginnen oefenen op AVI 9.\n`,
-  leesdiploma : `${displayName.value} behaalde in januari ${bezittVnw} leesdiploma. Proficiat!`,
-  nvt : ``
-}
-
-const dictTafels = {
-  goed : `De maaltafels en oefeningen van het studeerblad rekenen zijn goed gekend. ${aanwijzendVnwCap} mag ze af en toe blijven opfrissen, zodat deze vlot kunnen gebruikt worden tijdens de lessen wiskunde.\n`,
-  studeerblad_herhalen : `De oefeningen van het studeerblad zijn niet meer goed gekend. Het is nodig om deze oefeningen goed te kennen, zodat ze vlot kunnen toegepast worden tijdens de lessen wiskunde. Regelmatig opnieuw studeren en herhalen is dus belangrijk.\n`,
-  maaltafels_niet_gekend : `De maaltafels zijn niet meer goed gekend. Het is nodig om deze oefeningen goed te kennen, zodat ze vlot kunnen toegepast worden tijdens de lessen wiskunde. Regelmatig opnieuw studeren en herhalen is dus belangrijk.\n`,
-  studeerblad_maaltafels_niet_gekend : `De oefeningen van het studeerblad zijn niet meer goed gekend. Het is nodig om deze oefeningen goed te kennen, zodat ze vlot kunnen toegepast worden tijdens de lessen wiskunde. Regelmatig opnieuw studeren en herhalen is dus belangrijk.\n`
-}
-
-const dictVoorlezen = {
-  goed : `Tijdens het eten in de klas, lees ik elke dag voor uit het boek Mathilda. ${displayName.value} geniet dan volop van dit voorleesmoment.\n`,
-  nvt : ``
-}
-
-const dictSlot = {
-  goed : `De komende periode gaan we weer veel nieuwe dingen leren en ontdekken. Ik kijk er naar uit om ${displayName.value} verder te zien groeien.`,
-  kan_beter : `${displayName.value}, laat de komende periode zien wat je kan!`
-}
+  const dictKomma = {
+    goed_ook_maten : "Zo rekenden we voor de eerste keer met kommagetallen. Bewerkingen met tienden, honderdsten en duizendsten gaan voorlopig heel vlot voor (naam). Zelfs het herleiden van kommagetallen bij lengte- en inhoudsmaten lukt zonder veel moeite. Knap!\n",
+    goed_maten_minder : "Zo rekenden we voor de eerste keer met kommagetallen. Bewerkingen met tienden, honderdsten en duizendsten gaan voorlopig heel vlot voor (naam). Kommagetallen omzetten met een herleidingstabel is soms nog wel een uitdaging. Zet steeds de eenheden als eerste op de juiste plaats, dan volgt de rest wel. Als (naam) goed mee oefent tijdens de lessen, kan (hij) tegen het einde van het schooljaar een echte kommagetallenkampioen worden.\n",
+    matig : "Zo rekenden we voor de eerste keer met kommagetallen. Dat is nog niet zo eenvoudig. (naam) doet goed zijn best. Soms gaat het al heel goed, soms is het nog wat zoeken. (Hij) moet goed kijken welke plaats na de komma welke waarde heeft. Bij het optrekken en aftrekken is het handig om steeds evenveel cijfers na de komma te gebruiken. Als (naam) goed mee oefent tijdens de lessen, kan (hij) tegen het einde van het schooljaar een echte kommagetallenkampioen worden.\n",
+    moeilijk : "Zo rekenden we voor de eerste keer met kommagetallen. Dat is nog niet zo eenvoudig. (naam) moet goed kijken welke plaats na de komma welke waarde heeft. Bij het optrekken en aftrekken is het handig om steeds evenveel cijfers na de komma te gebruiken. We gaan hier de komende weken nog veel op oefenen.\n" 
+  }
 
   // Combine selected values to create a temporary string to output
   let result = "De derde periode van dit schooljaar zit er weeral op. Ook dit keer hebben we heel wat nieuwe dingen geleerd.\n";
 
-  result += dictSamenwerken[samenwerken] + dictIndividueel[individueel] + dictUitleg[uitleg] + dictKlassikaal[klassikaal] + dictKlGroepje[klGroepje] + dictHelpen[helpen] + dictSchrijven[schrijven] + dictBank[bank] + dictPunten[punten] + werkpunten + dictAvi[avi] + dictTafels[tafels] + dictVoorlezen[voorlezen] + dictSlot[slot];
+  result += dictKomma[kommagetallen];
 
   // Output result to webpage
   commentPara.innerHTML = result;
@@ -151,34 +56,4 @@ const dictSlot = {
 
 function returnSelection(btnSelector) {
     return document.querySelector(`input[name="${btnSelector}"]:checked`).value;
-}
-
-function getWerkpunten() {
-  let werkpuntenLijst = document.querySelectorAll("input[name='werkpunten']:checked");
-  return werkpuntenLijst;
-}
-
-function getWerkpuntenFullText(nodelist) {
-  // Define dictionary with feedback
-  let selectWerkpuntenBeetje1 = document.getElementById(`werkpunten_beetje_selector1`).value;
-  let selectWerkpuntenBeetje2 = document.getElementById(`werkpunten_beetje_selector2`).value;
-  let selectWerkpuntWiskunde = document.getElementById("werkpunten_wiskunde_selector").value;
-  const dictWerkpunten = {
-    inzet : `Ik denk dat ${displayName.value} thuis nog iets meer moet werken voor school.`,
-    beetje : `De komende periode kan ${aanwijzendVnw} nog wat extra aandacht schenken aan ${selectWerkpuntenBeetje1} en ${selectWerkpuntenBeetje2}.`,
-    wiskunde : `${selectWerkpuntWiskunde} blijft een werkpunt. Het is belangrijk om voldoende uitleg te komen vragen in de klas.`,
-    spelling : `De dictees van ${displayName.value} zijn onvoldoende. Het is belangrijk om elke dag de woorden van het woordpakket te schrijven en fouten te verbeteren. Op de website van de school vind je ingesproken dictees van elk woordpakket die kunnen helpen bij het oefenen.`,
-    wo_godsdienst : `${bezittVnwCap} resultaten voor w.o. en godsdienst zijn deze periode nogal zwak. Dit zijn onderdelen waar ${aanwijzendVnw} thuis voor moet studeren. ${aanwijzendVnwCap} moet dan elke dag leren en hoofdgedachten noteren.`
-  }
-
-  let fullText = "";
-  let length = nodelist.length;
-  console.log(length);
-  for (let i = 0; i < length; i++ ) {
-    console.log(nodelist[i].value);
-    fullText += dictWerkpunten[nodelist[i].value] + " ";
-  }
-  fullText += "\n";
-  console.log(fullText);
-  return fullText;
 }
