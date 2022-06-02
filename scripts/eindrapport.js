87e42bc706243f55baabda4eb28e025430460b36
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-06-02 21:10:33 +0200

    prevent form from refreshing page

--- scripts/eindrapport.js
+++ scripts/eindrapport.js
@@ -1,9 +1,10 @@
 // Initialize document elements
 const commentPara = document.getElementById("commentaartekst");
+const displayName = document.getElementById("name");
 
 // code for generate button
 const generateBtn = document.getElementById("generateButton");
-// generateBtn.addEventListener("click", generateBasisFeedback);
+// generateBtn.addEventListener("click", generateFeedback);
 
 // Set correct gendered words
 let aanwijzendVnw = "";
@@ -24,4 +25,13 @@ function setPronouns(gender) {
   }
   aanwijzendVnwCap = aanwijzendVnw.charAt(0).toUpperCase() + aanwijzendVnw.slice(1);
   bezittVnwCap = bezittVnw.charAt(0).toUpperCase() + bezittVnw.slice(1);
+}
+
+function generateFeedback() {
+  console.log('generating content...')
+  commentPara.innerText = 'This is the content';
+}
+
+function returnSelection(btnSelector) {
+    return document.querySelector(`input[name="${btnSelector}"]:checked`).value;
 }
